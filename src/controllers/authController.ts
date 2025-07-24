35606600af6ca4631c9d0e20d560ca7f267da954
Kavindu Tharaka <157113740+tharakaug@users.noreply.github.com>
2025-07-24 23:13:25 +0530

    update register and login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,16 +1,3 @@
-// import { Request, Response } from "express";
-// import * as authService from '../services/auth.service';
-// export const authenticateUser = (req: Request, res: Response) => {
-//     const {username, password} = req.body;
-//     const authToken = authService.authenticateUser(username, password);
-//
-//     if (!authToken) {
-//         res.status(401).json({error: 'Invalid credentials'});
-//         return;
-//     }
-//     res.json(authToken);
-// }
-
 import {Request, Response} from 'express';
 import * as authService from "../services/auth.service"
 import {registerUser} from "../services/user.service";
